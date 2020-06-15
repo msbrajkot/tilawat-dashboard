614ae2ceb27b14e4449c3b3cf8ca8d14974644c7
zohair.amravatiwala <zohair.amravatiwala@hotmail.com>
2020-06-15 23:51:46 +0530

    Auto-generated commit

--- src/app/shared/models/Tilawat.ts
+++
@@ -1,9 +0,0 @@
-export class Tilawat {
-    _id? : string;
-    date: Date;
-    suratName: string;
-    startAyat: number;
-    endAyat: number;
-    ayatCount: number;
-    juzFinished: boolean;
-}
